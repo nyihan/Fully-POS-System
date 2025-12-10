76d917f3faaf7eb2ee808565d33dfd2b9eda2509
nyihan <37435263+nyihan@users.noreply.github.com>
2025-12-10 14:05:53 +0630

    Update service worker cache version to v6

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "smartpos-v5";
+const CACHE_NAME = "smartpos-v6";
 const ROOT = "/fpos";
 
 const FILES_TO_CACHE = [
@@ -13,31 +13,18 @@ const FILES_TO_CACHE = [
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((k) => {
-          if (k !== CACHE_NAME) return caches.delete(k);
-        })
-      )
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => { if (k !== CACHE_NAME) return caches.delete(k); })))
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((res) => {
-      return (
-        res ||
-        fetch(event.request).catch(() => caches.match(`${ROOT}/index.html`))
-      );
-    })
+    caches.match(event.request).then(res => res || fetch(event.request).catch(()=>caches.match(`${ROOT}/index.html`)))
   );
 });
